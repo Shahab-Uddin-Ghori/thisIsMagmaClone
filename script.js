3e77d730ffd0e3580bf3ed8fd8f06cbcb0a2f9ed
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-07-22 16:34:33 +0500

    page2 setup with gsap

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ navBtn.addEventListener("mouseout",(e)=>{
 
     navBtn.style.transform = ``;
 })
+
+let clutter = "";
+document.querySelector("#page2 h1").textContent.split(" ").forEach((dets)=>{
+    clutter += `<span>${dets}</span>`
+
+    document.querySelector("#page2 h1").innerHTML = clutter;
+})
+
+gsap.to("#page2 h1 span",{
+scrollTrigger:{
+    trigger: `#page2 h1 span`,
+    start: `top bottom`,
+    end: `bottom top`,
+    scroller: `#main`,
+    scrub: .2
+},
+
+    stagger:.01,
+    color:`#fff`
+
+})
